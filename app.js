30f5ebeeb7e4aa93492c246915a1eb195d3fa11f
edjr025 <96031438+edjr025@users.noreply.github.com>
2022-01-30 22:54:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    //configurações do carrosel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $("#hero-carousel").owlCarousel({
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    });
+
+
+    $("#top-movies-slide").owlCarousel({
+        items: 6,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        resposive:{
+            500:{
+                items: 3,
+            },
+            1280:{
+                items: 4,
+            },
+
+           1600:{
+                items: 6,
+            }
+        }
+    });
+
+    $(".movies-slide").owlCarousel({
+
+        items: 6,
+        dots: false,
+        nav: true,
+        navText: navText,
+        margin: 15,
+        resposive:{
+            500:{
+                items: 3,
+            },
+            1280:{
+                items: 4,
+            },
+
+           1600:{
+                items: 6,
+            }
+        }
+
+    })
+
+    
+})
+
